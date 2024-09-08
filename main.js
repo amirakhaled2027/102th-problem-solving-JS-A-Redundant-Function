b3a5c31c0feca8e20418fb6d7fb1ac745059a7aa
Amira Khaled <155392660+amirakhaled2027@users.noreply.github.com>
2024-09-08 19:43:17 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,17 @@
+//Write a function redundant that takes in a string str and returns a function that returns str.
+
+
+function redundant(str) {
+    return function() {
+        return str;
+    }
+}
+
+const f1 = redundant("apple");
+console.log(f1()); 
+
+const f2 = redundant("pear");
+console.log(f2()); 
+
+const f3 = redundant("");
+console.log(f3()); 
